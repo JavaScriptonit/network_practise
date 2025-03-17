b69679d0365eb8aa175615201cdde354d9c0463b
Andrey Shabunov <shabunovaa@yandex.ru>
2025-03-18 00:30:16 +0300

    update readme and add terraform and ansible files

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # network_practise
+
+```
+git add .
+git commit -m "update readme"
+git push -u origin main
+```
+
+
+1. cd /Users/andreyshabunov/network_practise/terraform
+2. terraform init
+3. TF_LOG=DEBUG terraform apply
+
+
+1. cd /Users/andreyshabunov/network_practise/ansible
+2. ansible-playbook -i inventory playbook.yml
---
+++ terraform/main.tf
@@ -0,0 +1,53 @@
+resource "serverspace_server" "servers" {
+  count          = 2
+  image          = "Ubuntu-22.04-X64"
+  name           = "network-server-${count.index + 1}"
+  location       = "ds1"
+  cpu            = 1
+  ram            = 1024
+  boot_volume_size = 40 * 640
+
+  nic {
+    network      = ""
+    network_type = "PublicShared"
+    bandwidth    = 50
+  }
+
+  nic {
+    network      = serverspace_isolated_network.my_net.id
+    network_type = "Isolated"
+    bandwidth    = 0
+  }
+
+  ssh_keys = [
+    resource.serverspace_ssh.terraform.id,
+  ]
+
+  connection {
+    host         = self.public_ip_addresses[0]
+    user         = "root"
+    type         = "ssh"
+    private_key  = file(var.pvt_key)
+    timeout      = "2m"
+  }
+}
+
+output "server_ips" {
+  value = serverspace_server.servers[*].public_ip_addresses[0]
+}
+
+resource "serverspace_isolated_network" "my_net" {
+  location       = "ds1"
+  name           = "my_net"
+  description    = "Example for Terraform"
+  network_prefix = "192.168.0.0"
+  mask           = 24
+}
+
+resource "serverspace_isolated_network" "my_subnet" {
+  location       = "ds1"
+  name           = "my_subnet"
+  description    = "Subnet for my isolated network"
+  network_prefix = "192.168.1.0"
+  mask           = 24
+}
